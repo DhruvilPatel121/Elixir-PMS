5b9a17e76a2e70d6707fac5c5f41e3529821d3c0
elixir-dhruvil <dhruvil.cloud12@gmail.com>
2025-04-11 18:01:36 +0530

    routes.jsx file updated

--- pms/src/routes/routes.jsx
+++ pms/src/routes/routes.jsx
@@ -7,17 +7,6 @@ import Leave from '../pages/Team/Leave';
 import Announcements from '../pages/Team/Announcements';
 import Events from '../pages/Events/Events';
 import Projects from '../pages/Projects/Projects';
-<<<<<<< HEAD
-import Tasks from '../pages/Tasks/Tasks';
-import Notes from '../pages/Notes/Notes';
-import Reports from '../pages/Reports/Reports';
-import Timesheets from '../pages/Reports/Timesheets';
-import HelpSupport from '../pages/Help&Support/HelpSupport';
-import Help from '../pages/Help&Support/Help';
-import HelpArticles from '../pages/Help&Support/HelpArticles';
-import Todo from '../pages/Todo/ToDo';
-=======
->>>>>>> 9734ba8 (Create Project component)
 
 
 export const routes = [
@@ -37,19 +26,6 @@ export const routes = [
     name: 'Projects'
   },
   {
-<<<<<<< HEAD
-    path: '/tasks',
-    component: Tasks,
-    name: 'Tasks'
-  },
-  {
-    path: '/notes',
-    component: Notes,
-    name: 'Notes'
-  },
-  {
-=======
->>>>>>> 9734ba8 (Create Project component)
     path: '/team',
     component: Team,
     name: 'Team',
@@ -81,38 +57,4 @@ export const routes = [
       }
     ]
   },
-  {
-    path: '/reports',
-    component: Reports,
-    name: 'Reports',
-    children: [
-      {
-        path: 'timesheets',
-        component: Timesheets,
-        name: 'Timesheets'
-      }
-    ]
-  },
-  {
-    path: '/helpsupport',
-    component: HelpSupport,
-    name: 'Help & Support',
-    children: [
-      {
-        path: 'help',
-        component: Help,
-        name: 'Help'
-      },
-      {
-        path: 'helparticles',
-        component: HelpArticles,
-        name: 'Help Articles'
-      }
-    ]
-  },
-  {
-    path: '/todo',
-    component: Todo,
-    name: 'ToDo'
-  }
 ];
